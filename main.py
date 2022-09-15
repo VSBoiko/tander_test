277beb4f47431ac90a10fcb63eb6ea247a03b550
Vlad Boiko <vladboiko1996@yandex.ru>
2022-09-16 01:56:04 +0300

    added example of using creating methods in ExcelWorker

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 
 from classes.Db import Db
-from classes.ExcelParser import ExcelParser
+from classes.ExcelWorker import ExcelWorker
 
 
 if __name__ == '__main__':
@@ -9,8 +9,7 @@ if __name__ == '__main__':
     if not db.does_db_exist():
         db.create_db()
 
-    # parser = ExcelParser("new_users.xlsx")
-    # new_users = parser.get_rows("Sheet1")
+    # new_users = ExcelParser.get_rows("new_users.xlsx", "Sheet1")
     # for user in new_users:
     #     region = db.get_region_by_name(user["region_name"].strip()).pop()
     #     city = db.get_city_by_name(user["city_name"].strip()).pop()
@@ -24,3 +23,25 @@ if __name__ == '__main__':
     #         email=user["email"].strip()
     #     )
 
+    # users = db.get_all_users()
+    # regions = db.get_all_regions()
+    # cities = db.get_all_cities()
+    #
+    # columns = [
+    #     "second_name",
+    #     "first_name",
+    #     "patronymic",
+    #     "region_name",
+    #     "city_name",
+    #     "phone",
+    #     "email"
+    # ]
+    # for key, user in enumerate(users):
+    #     region = list(filter(lambda x: x.get("id") == user.get("region_id"), regions)).pop()
+    #     city = list(filter(lambda x: x.get("id") == user.get("city_id"), cities)).pop()
+    #     users[key].update({
+    #         "region_name": region.get("region_name"),
+    #         "city_name": city.get("city_name"),
+    #     })
+    #
+    # ExcelWorker.create_user_xlsx("test.xlsx", "Sheet255", columns, users)
